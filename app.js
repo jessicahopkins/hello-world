c0892ed96e0ad20272ee6e953a9fad67255e99e5
Jessica Hopkins <jhopkins@mutualofenumclaw.com>
2023-10-12 14:02:23 -0700

    Added app.js file

---
+++ app.js
@@ -0,0 +1,22 @@
+function getPetName(){
+    let petName = prompt('What is your pet\'s name?');
+    return petName;
+}
+
+function greetPet(name){
+    document.write('Welcome ' + name + '!');
+}
+
+function petLove(){
+    let loveAnswer = prompt('Do you love your pet? Enter 1 for YES or 2 for NO');
+    return loveAnswer
+}
+
+function specialMessage(love){
+    if(love == 1){
+        document.write('You are a great pet parent!');
+      } else if (love == 2){
+        document.write('Leave my page!');
+        }
+}
+    
